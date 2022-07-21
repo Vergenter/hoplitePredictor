2e06e1896129837ae4cab7d325272d9505d8ff7f
Vergenter <okolok1998@gmail.com>
2022-07-21 19:04:45 +0200

    prevent from duplicating unique objects

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
+
+const EMPTY_IMG = "data:image/gif;base64,R0lGODlhAQABAAAAACwAAAAAAQABAAA="
+const ASSETS = ["assets/ground.png","assets/player_moved.png","assets/magma.png","assets/altar_on.png","assets/footman.png","assets/archer.png","assets/wizard_discharged.png","assets/wizard_charged.png","assets/demolitionist_without_bomb_I.png","assets/demolitionist_without_bomb_II.png","assets/demolitionist_holding_bomb.png","assets/bomb.png","assets/fleece.png","assets/portal.png","assets/spear.png","assets/stairs.png"]
+const LIMITED_COUNT_ASSETS = ["assets/player_moved.png","assets/spear.png","assets/stairs.png","assets/altar_on.png","assets/fleece.png","assets/portal.png"]
 /**
  * 
  * @type {Array<string>}
  */
-const EMPTY_IMG = "data:image/gif;base64,R0lGODlhAQABAAAAACwAAAAAAQABAAA="
-const ASSETS = ["assets/ground.png","assets/player.png","assets/spear.png","assets/stairs.png","assets/magma.png","assets/altar_on.png","assets/fleece.png","assets/portal.png","assets/demolitionist_without_bomb_I.png","assets/demolitionist_without_bomb_II.png","assets/demolitionist_holding_bomb.png","assets/bomb.png","assets/archer.png","assets/wizard_discharged.png","assets/wizard_charged.png","assets/footman.png"]
+const limited_count_assets_taken = ["assets/stairs.png"]
 const IDICES_OUTSIDE_OF_MAP = [1,2,10,11,3,4,6,7,8,9,17,18,82,92,93,91,97,98,90,99].map(x=>x-1)
 const mapStates = Array.from({length: 99}, (_,i) => IDICES_OUTSIDE_OF_MAP.includes(i)?EMPTY_IMG:ASSETS[0])
+mapStates[13]="assets/stairs.png"
+/**
+ * @param {string} currentAssetPath
+ * @returns {string}
+ */
+function getNextAsset(currentAssetPath){
+    if (ASSETS.indexOf(currentAssetPath) ===-1) throw "currentAssetPath is not know asset path!"
+    const indexOfCurrentAsset = limited_count_assets_taken.indexOf(currentAssetPath)
+    if (indexOfCurrentAsset!==-1){
+        limited_count_assets_taken.splice(indexOfCurrentAsset,1)
+    }
+    let next = currentAssetPath
+    do{
+        next = ASSETS[(ASSETS.indexOf(next)+1)%ASSETS.length]
+    } while(limited_count_assets_taken.indexOf(next)!==-1)
+
+    if (LIMITED_COUNT_ASSETS.indexOf(next)!==-1){
+        limited_count_assets_taken.push(next)
+    }
+    return next
+}
+
 /**
  * @param {HTMLImageElement} img
  */
 function changeImage(img){
     const index = parseInt(img.id.split("_")[1])-1
     if (IDICES_OUTSIDE_OF_MAP.includes(index)) return
-    mapStates[index] =  ASSETS[(ASSETS.indexOf(mapStates[index])+1)%ASSETS.length]
+    mapStates[index] =  getNextAsset(mapStates[index])
     img.src = mapStates[index]
 }
